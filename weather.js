e089cca5d53f6ca6691684830148513968facaf9
shAriqsh <110338467+shAriqsh@users.noreply.github.com>
2023-04-08 08:14:40 +0530

    Update weather.js

--- weather.js
+++ weather.js
@@ -5,9 +5,16 @@ const apiDetails = {
 
 const input = document.querySelector("input");
 input.addEventListener("keypress", showData);
+let locationCity = document.querySelector(".location-city");
+let date = document.querySelector(".location-date");
+let tempp = document.querySelector(".temperature-temp");
+let type = document.querySelector(".temperature-type");
+let lowHigh = document.querySelector(".temperature-range");
+let hum = document.querySelector(".humidity");
 
 function showData(e) {
   if (e.keyCode === 13) {
+    console.log(input.value);
     showResults(input.value);
   }
 }
@@ -19,12 +26,16 @@ async function showResults(value) {
     return;
   }
   const data = await fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=${value}&units=metric&APPID=60bbd59ec7556e88c0f6b5a2080aebaa`
+    `${apiDetails.url}weather?q=${value}&units=metric&APPID=${apiDetails.api_key}`
   );
 
   const fdata = await data.json();
-  if (fdata.cod === "404") {
-    infoBox.innerText = `CITY NOT FOUND`;
+  if (fdata.message === "city not found") {
+    infoBox.style.display = "flex";
+    infoBox.style.flexDirection = "column";
+    infoBox.style.justifyContent = "center";
+    infoBox.style.alignItems = "center";
+    infoBox.innerHTML = `CITY NOT FOUND`;
   } else {
     infoBox.style.display = "flex";
     infoBox.style.flexDirection = "column";
@@ -39,14 +50,8 @@ function cls() {
   document.querySelector(".box").style.display = "none";
   return;
 }
-
 function displayData(data) {
-  const locationCity = document.querySelector(".location-city");
-  const date = document.querySelector(".location-date");
-  const tempp = document.querySelector(".temperature-temp");
-  const type = document.querySelector(".temperature-type");
-  const lowHigh = document.querySelector(".temperature-range");
-  const hum = document.querySelector(".humidity");
+  console.log(data);
 
   let time = new Date();
 
@@ -54,7 +59,6 @@ function displayData(data) {
   let pdate = time.getDate();
   let month = time.toLocaleString("en-IN", { month: "long" });
   let year = time.getFullYear();
-
   locationCity.innerText = `${data.name} (${data.sys.country})`;
 
   date.innerText = `${day} ${pdate} ${month} ${year}`;
